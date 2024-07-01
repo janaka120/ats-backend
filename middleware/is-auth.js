53ac05eb5279b6bdcbe01d50a4ec1a25916248df
janaka120 <janakastill@gmail.com>
2024-07-02 00:22:33 +0800

    Config auth middleware

---
+++ middleware/is-auth.js
@@ -0,0 +1,10 @@
+module.exports = (req, res, next) => {
+  const authHeader = req.get('Authorization');
+  if (!authHeader) {
+    const error = new Error('Not authenticated.');
+    error.statusCode = 401;
+    throw error;
+  }
+  // TODO: decode auth token and verify
+  next();
+};
